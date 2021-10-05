92c36f7098d8d5d9720e6ec2a855814a78d67950
yxzhao990302 <89767817+yxzhao990302@users.noreply.github.com>
2021-10-06 03:47:34 +0800

    Update predictor.py

--- prediction-cp/resources/predictor.py
+++ prediction-cp/resources/predictor.py
@@ -8,12 +8,11 @@ from keras.models import load_model
 def predict(dataset):
     model_repo = os.environ['MODEL_REPO']
     if model_repo:
-        file_path = os.path.join(model_repo, "model.h5")
+        file_path = os.path.join(model_repo, "model1.h5")
         model = load_model(file_path)
         val_set2 = dataset.copy()
         result = model.predict(dataset)
-        y_classes = result.argmax(axis=-1)
-        val_set2['class'] = y_classes.tolist()
+        val_set2['salary'] = result.tolist()
         dic = val_set2.to_dict(orient='records')
         return jsonify(dic), 200
     else:
