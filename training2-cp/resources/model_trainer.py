794d5fec6b3fe759d31ca8dc391e285b3da596af
yxzhao990302 <89767817+yxzhao990302@users.noreply.github.com>
2021-10-08 05:03:54 +0800

    Update model_trainer.py

--- training2-cp/resources/model_trainer.py
+++ training2-cp/resources/model_trainer.py
@@ -7,28 +7,29 @@ from flask import jsonify
 from keras.layers import Dense
 from keras.models import Sequential
 from sklearn.linear_model import Ridge
+import pickle
 
 
 def train(dataset):
     # split into input (X) and output (Y) variables
-    X = dataset.iloc[:, :-1].values
-    y = dataset.iloc[:, 1].values
+    X = dataset[:,0].reshape(-1,1)
+    y = dataset[:,1]
 
     # Splitting the dataset into the Training set and Test set
     #from sklearn.model_selection import train_test_split
 
     # Fitting Simple Linear Regression to the Training set
     #from sklearn.linear_model import LinearRegression, Ridge
-    regressor1 = Ridge(alpha=10)
-    regressor1.fit(X, y)
-    text_out ={"model built successfully"}
+    model = Ridge(alpha=10)
+    model.fit(X, y)
+    text_out ={"message": 'model built successfully'}
     
     model_repo = os.environ['MODEL_REPO']
     if model_repo:
-        file_path = os.path.join(model_repo, "model2.h5")
-        model.save(file_path)
+        file_path = os.path.join(model_repo, "model2.pickle")
+        pickle.dump(model, open(file_path, 'wb'))
         logging.info("Saved the model to the location : " + model_repo)
         return jsonify(text_out), 200
     else:
-        model.save("model2.h5")
+        pickle.dump(model, open(file_path, 'wb'))
         return jsonify({'message': 'The model was saved locally.'}), 200
