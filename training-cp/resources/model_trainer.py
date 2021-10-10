f8cf8ee6959875c50b740d566210864ec76cf2e7
yxzhao990302 <89767817+yxzhao990302@users.noreply.github.com>
2021-10-10 20:50:12 +0800

    Update model_trainer.py

--- training-cp/resources/model_trainer.py
+++ training-cp/resources/model_trainer.py
@@ -23,12 +23,24 @@ def train(dataset):
     model = LinearRegression()
     model.fit(X, Y)
     
-    model_repo = os.environ['MODEL_REPO']
+    project_id = os.environ.get('PROJECT_ID', 'Specified environment variable is not set.')
+    model_repo = os.environ.get('MODEL_REPO', 'Specified environment variable is not set.')
     if model_repo:
-        file_path = os.path.join(model_repo, "model.pickle")
-        pickle.dump(model, open(file_path, 'wb'))
-        logging.info("Saved the model to the location : " + model_repo)
+        pickle.dump(model, open('local_model1.pickle', 'wb'))
+        client = storage.Client(project=project_id)
+        bucket = client.get_bucket(model_repo)
+        blob = bucket.blob('model1.pickle')
+         # Upload the locally saved model
+        blob.upload_from_filename('local_model1.pickle')
+        os.remove('local_model1.pickle')
+        logging.info("Saved the model to GCP bucket : " + model_repo)
         return jsonify({'message': 'model was built successfully.'}), 200
-    else:
-        pickle.dump(model, open(file_path, 'wb'))
+        
+        
+        
+       
+       
+        
+     else:
+        pickle.dump(model, open('local_model1.pickle', 'wb'))
         return jsonify({'message': 'The model was saved locally.'}), 200
